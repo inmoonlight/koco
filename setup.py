94b9891f1c8efca5020211e79286e1f848ce466b
inmoonlight <mjihyung@gmail.com>
2020-05-20 23:51:27 +0900

    Add long-description for pypi and Bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages, setup
 
 REQ_FILE = 'requirements.txt'
-VERSION = '0.2.0'
+VERSION = '0.2.2'
 
 
 def get_requires():
@@ -15,6 +15,8 @@ setup(
     name='koco',
     version=VERSION,
     description='A library to easily access kocohub datasets',
+    long_description=open('README.md', 'r', encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     author='Jihyung Moon',
     author_email='mjihyung@gmail.com',
     url='https://github.com/inmoonlight/koco',
